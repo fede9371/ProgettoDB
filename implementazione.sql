153f5d98a414a09080c67245776c0f243d8698f6
tode3007 <38936369+tode3007@users.noreply.github.com>
2020-02-11 14:03:39 +0100

    Update implementazione.sql

--- implementazione.sql
+++ implementazione.sql
@@ -4,7 +4,8 @@ drop database azienda cascade;
 -- COPIARE DA QUI IN POI PER CREARE LE TABELLE LA PRIMA VOLTA --
 ----------------------------------------------------------------
 
-create database azienda; 
+create database azienda;
+use azienda;
 
 create domain dom_cf as varchar check(value ~ '[A-Z]{6}[0-9]{2}[A-Z]{1}[0-9]{2}[A-Z][0-9]{3}[A-Z]');
 
@@ -100,13 +101,15 @@ CREATE TABLE articolo_venduto(
 CREATE TABLE partita (
  codice dom_partita primary key,
  quantita integer not null,
+ qualita_scelta varchar(7) check (qualita_scelta in ('prima', 'seconda','terza')) not null,
  bio boolean not null,
  razza varchar not null,
  tipo varchar not null,
  costo_acquisto numeric not null,
  costo_stoccaggio numeric not null,
  costo_spedizione numeric not null,
- p_iva dom_piva unique);
+ p_iva dom_piva unique
+ );
 
 CREATE TABLE articolo_comprato (
  codice dom_articoloComp ,
@@ -120,141 +123,67 @@ CREATE TABLE articolo_comprato (
 Aggiunta chiavi esterne:
 ATTENZIONE:controllare on update e on delete
 
-ALTER TABLE agente
-ADD CONSTRAINT constraint_fk 
-FOREIGN KEY (capo_area) 
-REFERENCES capo_area (cf)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE agente ADD CONSTRAINT constraint_fk FOREIGN KEY (capo_area) REFERENCES capo_area (cf) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE capo_area 
-ADD CONSTRAINT constraint_fk 
-FOREIGN KEY (mercato)
-REFERENCES mercato (nome)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE capo_area ADD CONSTRAINT constraint_fk FOREIGN KEY (mercato) REFERENCES mercato (nome) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE regione 
-ADD CONSTRAINT constraint_fk 
-FOREIGN KEY (mercato) 
-REFERENCES mercato (nome)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE regione ADD CONSTRAINT constraint_fk FOREIGN KEY (mercato) REFERENCES mercato (nome) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE macro_zona
-ADD CONSTRAINT constraint_fk 
-FOREIGN KEY (mercato) 
-REFERENCES mercato (nome)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE macro_zona ADD CONSTRAINT constraint_fk FOREIGN KEY (mercato) REFERENCES mercato (nome) ON DELETE SET NULL ON UPDATE CASCADE;
 
 //Tabella formazione
-ALTER TABLE formazione 
-ADD CONSTRAINT constraint_fk_macro_zona 
-FOREIGN KEY (macro_zona) 
-REFERENCES macro_zona (mercato)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE formazione ADD CONSTRAINT constraint_fk_macro_zona FOREIGN KEY (macro_zona) REFERENCES macro_zona (mercato) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE formazione 
-ADD CONSTRAINT constraint_fk_regione 
-FOREIGN KEY (regione)
-REFERENCES regione (mercato)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE formazione ADD CONSTRAINT constraint_fk_regione FOREIGN KEY (regione) REFERENCES regione (mercato) ON DELETE CASCADE ON UPDATE CASCADE;
 
 //Tabella cliente
-ALTER TABLE cliente
-ADD CONSTRAINT constraint_fk_mercato 
-FOREIGN KEY (mercato) 
-REFERENCES mercato (nome)
-ON DELETE NO ACTION
-ON UPDATE CASCADE;
+ALTER TABLE cliente ADD CONSTRAINT constraint_fk_mercato FOREIGN KEY (mercato) REFERENCES mercato (nome) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 
-ALTER TABLE cliente
-ADD CONSTRAINT constraint_fk_agente
-FOREIGN KEY (agente) 
-REFERENCES agente (cf)
-ON DELETE NO ACTION
-ON UPDATE CASCADE;
+ALTER TABLE cliente ADD CONSTRAINT constraint_fk_agente FOREIGN KEY (agente) REFERENCES agente (cf) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 //Tabella sede
-ALTER TABLE sede 
-ADD CONSTRAINT constraint_fk 
-FOREIGN KEY (cliente) 
-REFERENCES cliente (p_iva)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE sede ADD CONSTRAINT constraint_fk FOREIGN KEY (cliente) REFERENCES cliente (p_iva)  ON DELETE CASCADE ON UPDATE CASCADE;
 
 //Tabella Ordine
-ALTER TABLE ordine 
-ADD CONSTRAINT constraint_fk 
-FOREIGN KEY (cliente) 
-REFERENCES cliente (p_iva)
-ON DELETE NO ACTION
-ON UPDATE CASCADE;
+ALTER TABLE ordine ADD CONSTRAINT constraint_fk FOREIGN KEY (cliente) REFERENCES cliente (p_iva) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 
 //Tabella articolo_venduto
-ALTER TABLE articolo_venduto 
-ADD CONSTRAINT constraint_fk_ordine
-FOREIGN KEY (ordine) 
-REFERENCES ordine (n_fattura)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE articolo_venduto ADD CONSTRAINT constraint_fk_ordine FOREIGN KEY (ordine) REFERENCES ordine (n_fattura) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE articolo_venduto 
-ADD CONSTRAINT constraint_fk_linea_prodotto
-FOREIGN KEY (linea_prodotto) 
-REFERENCES linea_prodotto (nome)
-ON DELETE NO ACTION
-ON UPDATE CASCADE;
+ALTER TABLE articolo_venduto ADD CONSTRAINT constraint_fk_linea_prodotto FOREIGN KEY (linea_prodotto) REFERENCES linea_prodotto (nome) ON DELETE NO ACTION ON UPDATE CASCADE;
 
-ALTER TABLE articolo_venduto
-ADD CONSTRAINT constraint_fk_articolo_comprato
-FOREIGN KEY (articolo_comprato,partita) 
-REFERENCES articolo_comprato (codice,partita)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE articolo_venduto ADD CONSTRAINT constraint_fk_articolo_comprato FOREIGN KEY (articolo_comprato,partita) REFERENCES articolo_comprato (codice,partita) ON DELETE SET NULL ON UPDATE CASCADE;
 
 
 //Tabella articolo_comprato
-ALTER TABLE articolo_comprato 
-ADD CONSTRAINT constraint_partita 
-FOREIGN KEY (partita) 
-REFERENCES partita (codice)
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE articolo_comprato ADD CONSTRAINT constraint_partita FOREIGN KEY (partita) REFERENCES partita (codice) ON DELETE SET NULL ON UPDATE CASCADE;
 
 //Tabella partita
-ALTER TABLE partita 
-ADD CONSTRAINT constraint_p_iva 
-FOREIGN KEY (p_iva)
-REFERENCES fornitore (p_iva)
-ON DELETE NO ACTION
-ON UPDATE CASCADE;
+ALTER TABLE partita ADD CONSTRAINT constraint_p_iva FOREIGN KEY (p_iva) REFERENCES fornitore (p_iva) ON DELETE NO ACTION ON UPDATE CASCADE;
 
+All’interno della base di dati sono presenti altri tipi di vincoli che implicano la creazione di appositi trigger per essere rispettati:
+-la data di scadenza di un articolo non deve essere posteriore alla data in cui viene effettuato un ordine con quell’articolo
+-quando inserisco un cliente in un mercato, l’agente che lo segue deve essere coordinato dal capo area che gestisce quel mercato
+-il costo totale di un ordine deve essere uguale alla quantità di ciascun articolo venduto contenuto moltiplicato per il suo prezzo
+-il prezzo di ogni articolo comprato deve essere uguale alla somma dei costi di acquisto, spedizione e stoccaggio, divisa per la quantità di articoli comprati presenti nella partita
+-un ORDINE da parte del CLIENTE non può avvenire in una data antecedente a quella attuale (questo vale per la modifica della data dell'ordine)
 
-POSSIBILI TRIGGER:
-1)Tabella articolo_venduto: data_scadenza>data ordine
-2)Tabella ordine: costo_totale = quantità * prezzo articolo (perchè attributo derivato)
-3)Tabella cliente: quando inserisco un cliente in un mercato, l’agente che lo segue deve essere coordinato dal capo area che gestisce quel mercato
-4) Tabella ordine:Data di ordine < data odierna
-5) Tabella articolo_comprato: prezzo = (costo acquisto + spedizione + stoccaggio) \ quantità 
 
 
 Vincoli risolvibili con l’aggiunta di not null nella tabella:
 -Tabella cliente: un cliente deve avere almeno un recapito telefonico ed almeno una mail
+ 
+ Di seguito sono riportati altri vincoli di integrità ancora da implementare:
+-un cliente deve avere almeno un recapito telefonico ed almeno una mail
+-un cliente deve afferire ad un unico mercato, il\i numero\i di telefono e gli indirizzi mail devono essere unici
+-un capo area deve gestire un unico mercato
+-un agente deve essere coordinato da un unico capo-area
+-uno specifico ordine è effettuato da un unico cliente
+-ogni articolo_venduto deve appartenere ad un unico ordine e ad una linea di prodotto unica
+-ogni partita è fornita da un unico fornitore
 
-Vincoli risolvibili con l’aggiunta di unique nelle tabelle:
--Tabella cliente: un cliente deve afferire ad un unico mercato,tel e mail unici
--Tabella capo-area: un capo area deve gestire un unico mercato 
--Tabella agente: un agente deve essere coordinato da un unico capo-area
--Tabella ordine: uno specifico ordine è effettuato da un unico cliente
--Tabella articolo_venduto: ogni articolo_venduto deve appartenere ad un unico ordine e ad una linea di prodotto unica
--Tabella partita: ogni partita è fornita da un unico fornitore
--Tabella cliente: ogni tel e\o mail deve essere univoco\a
 
 Gli altri sono tutti uguali e si possono risolvere allo stesso modo
 
@@ -264,14 +193,7 @@ Data di ordine < data odierna
 
 Constraint numero 1: Data di confezionamento < data_scadenza
 
-create  or  replace  function
-check_date_valide(data_conf  date , data_scad  date)
-returns  bool as
-$$
-  begin
-    return  data_conf < data_scad;
-   end;
-$$  language  plpgsql;
+create or replace function check_date_valide(data_conf  date , data_scad  date) returns bool language plpgsql as $$     begin     return  data_conf < data_scad; end; $$ ;
 
 alter  table  articolo_venduto add
  constraint  check_date_valide_ric
@@ -279,42 +201,28 @@ alter  table  articolo_venduto add
 
 Constraint numero 2: Data di ordine < data odierna
 
-create  or  replace  function
-check_date_valide_ordine(data_ordine date)
-returns  bool as
-$$
-  begin
-    return  data_ordine < current_date
-   end;
-$$  language  plpgsql;
+create or replace function check_date_valide_ordine(data_ordine date) returns bool language plpgsql as $$     begin   return data_ordine < current_date; end; $$ ;
+
 
 alter  table  ordine add
  constraint  check_date_valide_ordine_ric
   check(check_date_valide_ordine(data));
 
+Il vincolo 1, diversamente dai precedenti vincoli sulle date, necessita di un trigger perché coinvolge attributi di tabelle diverse. 
+Il codice seguente deve essere eseguito quando sarà inserito nella base un nuovo ARTICOLO_COMPRATO o quando se ne modifica la data di Scadenza.
 
 
 
-
-
-
-
-Trigger 1: Tabella articolo_venduto: data_scadenza>data ordine
-Da eseguire quando si inserisce un nuovo articolo_venduto
-O quando si modifica la data di scadenza 
-
-create  or  replace  function 
-check_dateOrdine(fattura dom_fattura,dataScadenza date)
-returns trigger as
-$$ 
-begin
+create  or  replace  function check_dateOrdine(fattura dom_fattura,dataScadenza date) returns trigger as
+ $$ 
+  begin
   perform *
   from ordine
-  where n_fattura=fattura and ordine.data<dataScadenza
+  where n_fattura=fattura and ordine.data< dataScadenza
 if found
     then
 raise  exception
-       ’Non è possibile fare vendere un articolo scaduto’;
+       ’Non è possibile vendere un articolo scaduto’;
  return  null;
      else
 return  new;
@@ -327,62 +235,12 @@ create  trigger modifica_ordine before update
    when (new.scadenza <> old.scadenza)
   execute  procedure check_dateOrdine(ordine,scadenza);
 
-
-
-
-
 create  trigger  newOrdine before  insert
   on  articolo_venduto for  each  row
   execute  procedure check_dateOrdine(ordine,scadenza);
 
-
-
-
-
-
-Trigger 2: Tabella ordine: costo_totale = quantità * prezzo articolo (perchè attributo derivato)
-Da eseguire quando si inserisce un nuovo ordine o quando si modifica il costo_totale 
-
-create  or  replace  function
-check_costo(fattura dom_fattura)
-returns  bool as
-$$ 
-begin
-  perform *
-  from articolo_venduto
-  where  fattura=ordine and costo_totale = prezzo_articolo*quantita
-if not found
-    then
-raise  exception
-       ’E’ sbagliato il calcolo del costo_totale ’;
-return  null;
-     else
-return  new;
-  end if;
-end;
-
-  $$  language  plpgsql;
-
-create  trigger  costo_ordine before  insert
-  on  ordine for  each  row
-  execute  procedure check_costo(n_fattura);
-
-
-create  trigger new_costo_ordine before update
-  on  ordine for  each  row
-   when ( new.costo_totale  <> old.costo_totale)
-  execute  procedure check_costo(n_fattura);
-
-
-
-
-
-
-
-
-Trigger 3: Tabella cliente: quando inserisco un cliente in un mercato, l’agente che lo segue deve essere coordinato dal capo area che gestisce quel mercato.
-Da eseguire quando si inserisce un nuovo cliente oppure si modifica l’agente che segue il cliente o il mercato di appartenenza del cliente 
-
+Il vincolo 2 è necessario per garantire che non si creino inconsistenze nel ciclo trattato nella Sezione 3.3.2.
+Il codice seguente deve essere eseguito quando si inserisce un nuovo cliente oppure si modifica l’agente che segue il cliente o il mercato di appartenenza del cliente.
 
 create  or  replace  function 
 check_cliente(cfAgente dom_cf,nomeMercato varchar(25))
@@ -396,8 +254,7 @@ begin
                     from capo_area
                     where agente.capo_area=capo_area.cf
                           and capo_area.mercato <> nomeMercato)
-
-if not found
+if found
     then
 raise  exception
        ’Non rispetta i vincoli di integrità ’;
@@ -407,20 +264,24 @@ return  new;
   end if;
 end;
 
-
 $$  language  plpgsql;
 
 create  trigger  newCliente before insert
   on  cliente for  each  row
-  execute  procedure check_cliente(agente,mercato);
+  execute  procedure check_cliente(agente, mercato);
 
 create  trigger cliente_modifica before update
   on  cliente for  each  row
    when ( new.agente  <> old.agente  or
             new.mercato  <> old.mercato)
-  execute  procedure check_cliente(agente,mercato);
+  execute  procedure check_cliente(agente, mercato);
 
-Trigger 4:
+
+
+
+
+
+
 Data di un ordine non deve essere successiva alla data odierna (considero data in cui viene fatto l’ordine e non data in cui viene consegnato al cliente). Viene eseguito quando modifico data dell’ordine 
 
 create  or  replace  function 
@@ -448,52 +309,172 @@ create  trigger modifica_ordine before update
   execute  procedure check_ordine(n_fattura);
 
 
+Per attributi derivati SQL non offre un costrutto, perciò è necessario creare dei trigger
+Attributi derivati :
+1)Tabella ordine: costo_totale = quantità * prezzo articolo (perchè attributo derivato)
+2)Tabella articolo_comprato: prezzo = (costo acquisto + spedizione + stoccaggio) \ quantità 
+
+Implementazione primo trigger:
+Trigger differenti per inserimento e aggiornamento
+Devo modificare il campo costo_totale di ordine quando:
+-Inserisco un nuovo articolo
+-Modifico il campo prezzo_articolo o quantita di articolo_venduto
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-Vincolo 5:
-Tabella articolo-comprato: prezzo = (costo acquisto + spedizione + stoccaggio) \ quantità 
-Viene eseguito quando modifico partita o prezzo di un articolo comprato o quando inserisco un nuovo articolo comprato 
+Definiamo prima la funzione che servirà per calcolare il costo_totale:
 
 
 create  or  replace  function 
-check_articolo(Numpartita dom_partita)
+calcolo_costo(NumFattura dom_fattura)
+returns float
+language  plpgsql as $$ 
+declare
+   quantita int:= 0;
+   prezzo float := 0;
+   costo float :=0;
+  
+begin
+  select av.prezzo_articolo as prezzo, av.quantita as quantita
+  from ordine as o, articolo_venduto as av
+  where av.ordine=o.n_fattura and o.n_fattura=NumFattura;
+return  costo=prezzo*quantita;
+end;
+$$ ;
+
+
+
+create  or  replace  function 
+insert_costo_ordine()
 returns trigger as
 $$ 
 begin
-  perform *
-  from partita as p,articolo_comprato
-  where codice=Numpartita and 
-      articolo_comprato.prezzo=(p.costo_acquisto+p.costo_stoccaggio+p.costo_spedizione)\quantita
-
-If not found found
-    then
-raise  exception
-       ’Errore di derivazione dell’attributo prezzo’;
- return  null;
-     else
+--inserimento nuovo ordine 
+  update ordine
+  set costo_totale= calcolo_costo(new.ordine)
+  where new.ordine=ordine.n_fattura
 return  new;
-  end if;
 end;
 $$  language  plpgsql;
 
-create  trigger  newArticolo before insert
-  on  articolo_comprato for  each  row
-  execute  procedure check_articolo(partita);
+create  or  replace  function 
+update_costo_ordine()
+returns trigger as
+$$ 
+begin
+  update ordine
+  set costo_totale= calcolo_costo(new.ordine)
+  where new.ordine-=ordine.n_fattura
+
+--se ho cambiato ordine devo ricalcolare il valore 
+
+if(new.ordine <> old.ordine)
+  then
+  update ordine
+  set costo_totale= calcolo_costo(old.ordine)
+  where old.ordine=ordine.n_fattura
+  end if;
+
+
+return  new;
+end;
+$$  language  plpgsql;
+
+
+
+create  trigger insert_ordine before insert
+  on  articolo_venduto for  each  row
+  execute  procedure insert_costo_ordine();
 
 create  trigger modifica_articolo before update
+  on  articolo_venduto for  each  row
+   when (new.prezzo_articolo <> old.prezzo_articolo or
+          new.quantita  <> old.quantita )
+  execute  procedure update_costo_ordine();
+
+2) Tabella articolo_comprato: prezzo = (costo acquisto + spedizione + stoccaggio) \ quantità 
+
+Implementazione secondo trigger:
+Trigger differenti per inserimento e aggiornamento
+Devo modificare il campo prezzo di articolo_comprato quando:
+-Inserisco un nuovo articolo
+-Modifico il campo costo_acquisto o costo_stoccaggio o costo_spedizione o quantita di partita
+
+Definiamo prima la funzione che servirà per calcolare il prezzo:
+
+
+
+create  or  replace  function 
+calcolo_prezzo_articolo(partita dom_partita)
+returns float
+language  plpgsql as $$ 
+declare
+   quantita int:= 0;
+   costo_acquisto float := 0;
+   costo_stoccaggio float :=0;
+   costo_spedizione float :=0;
+   prezzo_tot float :=0;
+begin
+  select p.quantita as quantita, p.costo_acquisto as costo_acquisto,
+         p.costo_stoccaggio as costo_stoccaggio, p.costo_spedizione as 
+         costo_spedizione  
+from partita as p, articolo_comprato as ac
+  where ac.partita=p.codice and ac.partita=partita;
+return prezzo_tot= (costo_spedizione + costo_stoccaggio + costo_acquisto)     \ quantita
+end;
+$$ ;
+
+
+
+create  or  replace  function 
+insert_prezzo_articolo()
+returns trigger as
+$$ 
+begin
+--inserimento nuovo articolo
+  update articolo_comprato
+  set prezzo=calcolo_prezzo_articolo(partita dom_partita)
+  where new.partita=articolo_comprato.partita
+return  new;
+end;
+$$  language  plpgsql;
+
+create  or  replace  function 
+update_prezzo_articolo()
+returns trigger as
+$$ 
+begin
+  update ordine
+  set prezzo=calcolo_prezzo_articolo(partita dom_partita)
+  where new.partita=articolo_comprato.partita
+
+--se ho cambiato ordine devo ricalcolare il valore 
+
+if(new.partita <> old.partita)
+  then
+  update articolo_comprato
+  set prezzo=calcolo_prezzo_articolo(partita dom_partita)
+  where old.partita=articolo_comprato.partita
+  end if;
+
+
+return  new;
+end;
+$$  language  plpgsql;
+
+create  trigger insert_articolo_comprato before insert
   on  articolo_comprato for  each  row
-   when (new.partita <> old.partita or
-          new.prezzo  <> old.prezzo )
-  execute  procedure check_articolo(partita);
+  execute  procedure insert_prezzo_articolo();
+
+create  trigger modifica_articolo_comprato before update
+  on  partita for  each  row
+   when (new.quantita <> old.quantita or
+         new.costo_spedizione  <> old.costo_spedizione or
+         new.costo_acquisto  <> old.costo_acquisto or
+         new.costo_stoccaggio <> old.costo_stoccaggio )
+  execute  procedure update_prezzo_articolo();
+
+
+
+
+
+
